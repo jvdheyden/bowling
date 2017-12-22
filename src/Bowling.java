af285925086a9387a61f7f5d96f807ef9009aa71
heydenjq <jonas.heyden@informatik.hu-berlin.de>
2017-12-22 12:15:44 +0100

    hack,hack

--- src/Bowling.java
+++ src/Bowling.java
@@ -1,5 +1,63 @@
+import java.util.*;
 
 public class Bowling
 {
+	private List<Frame> _frameList;
+	private List<String> _players;
+
+	public Bowling(List<String> players)
+	{
+		_frameList = new ArrayList<Frame>();
+		_players = players;
+	}
+
+	public static Bowling startGame(List<String> players)
+	{
+		return new Bowling(players);
+	}
+
+	public boolean isGameFinished()
+	{
+	}
+
+	public Set<String> winners()
+	{
+	}
+
+	public int score(String player)
+	{
+		int sum = 0;
+
+		for (Frame f : _frameList)
+		{
+			if (f.getPlayer().equals(player))
+			{
+				sum = sum + f.getScore();
+			}
+		}
+		return sum;
+	}
+
+	public int frameScore(String player, int frame)
+	{
+		int score = -1;
+		for (Frame f: _frameList)
+		{
+			if ((f.getPlayer() == player) && (f.getFrameNumber() == frame))
+			{
+				score = f.getScore();
+				break;
+			}
+		}
+		return score;
+	}
+
+	public String playsNext()
+	{
+	}
+
+	public void roll(int droppedPins)
+	{
+	}
 
 }
---
+++ src/Frame.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class Frame
+{
+
+	private String _player;
+	private int _frameNumber;
+	private List<Integer> _scores;
+
+	public Frame(String player, int frameNumber, List<Integer> scores)
+	{
+		_player = player;
+		_frameNumber = frameNumber;
+		_scores = scores;
+	}
+
+	public String getPlayer()
+	{
+		return _player;
+	}
+
+	public int getFrameNumber()
+	{
+		return _frameNumber;
+	}
+
+	public int getScore()
+	{
+		int sum = 0;
+		for (int i : _scores)
+		{
+			sum = sum + i;
+		}
+		return sum;
+	}
+
+}
